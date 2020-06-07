8462cd7c6b136fa1f3474d156abc28880b122c86
WeijieChen2017 <weijiechen1994@gmail.com>
2020-06-06 19:58:38 -0500

    v1

--- pytorch-CycleGAN-and-pix2pix/models/test_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/test_model.py
@@ -47,7 +47,8 @@ class TestModel(BaseModel):
         print(opt.netG)
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG,
                                       opt.norm, not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
-
+        print(opt.input_nc, opt.output_nc, opt.ngf, opt.netG,
+                                      opt.norm, not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
         # assigns the model to self.netG_[suffix] so that it can be loaded
         # please see <BaseModel.load_networks>
         setattr(self, 'netG' + opt.model_suffix, self.netG)  # store netG in self.
