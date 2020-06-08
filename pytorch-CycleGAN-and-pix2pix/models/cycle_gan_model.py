5a8c22c25e3b5095f5b780e83785945a1fd7b9b6
WeijieChen2017 <weijiechen1994@gmail.com>
2020-06-08 01:18:38 -0500

    v1

--- pytorch-CycleGAN-and-pix2pix/models/cycle_gan_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/cycle_gan_model.py
@@ -152,7 +152,9 @@ class CycleGANModel(BaseModel):
 
     def backward_G(self):
         """Calculate the loss for generators G_A and G_B"""
-        lambda_idt = self.opt.lambda_identity
+        # lambda_idt = self.opt.lambda_identity
+        # no idtLoss
+        lambda_idt = 0
         lambda_A = self.opt.lambda_A
         lambda_B = self.opt.lambda_B
         # Identity loss
